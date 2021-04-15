a12bf742a44d0e745edad4261a501227dd42cc46
ichiki-mfw <ichiki.kansei@moneyforward.co.jp>
2021-04-15 15:40:44 +0900

    step1

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
 
 type Reservation struct{
 	hour,min,minutes int
 }
 
-func main() {
+func s2i(s string) int {
+	i,err := strconv.Atoi(s)
+	if err!=nil{
+		panic(err)
+	}
+	return i
+}
 
-	t := Reservation{11,30,30}
-	fmt.Printf("開始時刻：%d:%d、終了時刻：%d:%d", t.hour, t.min, t.hour + int((t.min+t.minutes)/60), (t.min + t.minutes)%60)
+func main() {
+	nstring := nextLine() // 予約の数
+	n := s2i(nstring)
+	reservations := make([]*Reservation,0,n)
+	for i:=0; i<n; i++{
+		s := strings.Split(nextLine(), " ")
+		r := &Reservation{s2i(s[0]),s2i(s[1]),s2i(s[2])}
+		reservations = append(reservations,r)
+	}
+	for _,t := range reservations {
+		fmt.Printf("開始時刻：%02d:%02d、終了時刻：%02d:%02d\n", t.hour, t.min, t.hour+int((t.min+t.minutes)/60), (t.min+t.minutes)%60)
+	}
 }
