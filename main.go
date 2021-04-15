998313a8eed3f8645f5e4380281b2a08c3bb64ed
YuitoNakamori <nakamori.yuito@moneyforward.co.jp>
2021-04-15 15:28:19 +0900

    step1の出力

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type Reservation struct{
+	hour,min,minutes int
+}
+
 func main() {
-	fmt.Println("OK")
+
+	t := Reservation{11,30,30}
+	fmt.Printf("開始時刻：%d:%d、終了時刻：%d:%d", t.hour, t.min, t.hour + int((t.min+t.minutes)/60), (t.min + t.minutes)%60)
 }
